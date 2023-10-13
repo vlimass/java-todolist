84dadf675813b00a6d8ee427b3f6bf403a3649e6
Vinícius Lima <villylss@gmail.com>
2023-10-13 10:37:22 -0300

    feature: aula 4 concluída

--- src/main/java/br/com/vlimass/todolist/task/TaskController.java
+++ src/main/java/br/com/vlimass/todolist/task/TaskController.java
@@ -1,16 +1,21 @@
 package br.com.vlimass.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vlimass.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -39,4 +44,21 @@ public class TaskController {
     return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser"); 
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    // Copia todas as propriedades que não serão alteradas
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
+  }
 }
